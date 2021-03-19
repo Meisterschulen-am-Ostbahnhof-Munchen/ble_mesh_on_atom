5bc128fa55676650b1cc441568d1b3283656b396
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-03-19 23:12:04 +0100

    copy from
    https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/esp-ble-mesh/ble-mesh-faq.html#how-can-provisioner-get-and-parse-the-composition-data-of-nodes-through-configuration-client-model

--- decode_comp_data/main/main.c
+++ decode_comp_data/main/main.c
@@ -1,43 +1,94 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
-#include "sdkconfig.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
+#include <string.h>
+#include <stdint.h>
+
+//test date: 0C001A0001000800030000010501000000800100001003103F002A00
+//0C00 1A00 0100 0800 0300 0001 05 01 0000 0080 0100 0010 0310 3F002A00
+
+// CID is 0x000C
+// PID is 0x001A
+// VID is 0x0001
+// CRPL is 0x0008
+// Features is 0x0003 – Relay and Friend features.
+// Loc is “front” – 0x0100
+// NumS is 5
+// NumV is 1
+// The Bluetooth SIG Models supported are: 0x0000, 0x8000, 0x0001, 0x1000, 0x1003
+// The Vendor Models supported are: Company Identifier 0x003F and Model Identifier 0x002A
+
+typedef struct {
+    int16_t cid;
+    int16_t pid;
+    int16_t vid;
+    int16_t crpl;
+    int16_t features;
+    int16_t all_models;
+    uint8_t sig_models;
+    uint8_t vnd_models;
+} esp_ble_mesh_composition_head;
+
+typedef struct {
+    uint16_t model_id;
+    uint16_t vendor_id;
+} tsModel;
+
+typedef struct {
+    // reserve space for up to 20 SIG models
+    uint16_t SIG_models[20];
+    uint8_t numSIGModels;
+
+    // reserve space for up to 4 vendor models
+    tsModel Vendor_models[4];
+    uint8_t numVendorModels;
+} esp_ble_mesh_composition_decode;
+
+int decode_comp_data(esp_ble_mesh_composition_head *head, esp_ble_mesh_composition_decode *data, uint8_t *mystr, int size)
+{
+    int pos_sig_base;
+    int pos_vnd_base;
+    int i;
+
+    memcpy(head, mystr, sizeof(*head));
+
+    if(size < sizeof(*head) + head->sig_models * 2 + head->vnd_models * 4) {
+        return -1;
+    }
+
+    pos_sig_base = sizeof(*head) - 1;
+
+    for(i = 1; i < head->sig_models * 2; i = i + 2) {
+        data->SIG_models[i/2] = mystr[i + pos_sig_base] | (mystr[i + pos_sig_base + 1] << 8);
+        printf("%d: %4.4x\n", i/2, data->SIG_models[i/2]);
+    }
+
+    pos_vnd_base = head->sig_models * 2 + pos_sig_base;
+
+    for(i = 1; i < head->vnd_models * 2; i = i + 2) {
+        data->Vendor_models[i/2].model_id = mystr[i + pos_vnd_base] | (mystr[i + pos_vnd_base + 1] << 8);
+        printf("%d: %4.4x\n", i/2, data->Vendor_models[i/2].model_id);
+
+        data->Vendor_models[i/2].vendor_id = mystr[i + pos_vnd_base + 2] | (mystr[i + pos_vnd_base + 3] << 8);
+        printf("%d: %4.4x\n", i/2, data->Vendor_models[i/2].vendor_id);
+    }
+
+    return 0;
+}
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    esp_ble_mesh_composition_head head = {0};
+    esp_ble_mesh_composition_decode data = {0};
+    uint8_t mystr[] = { 0x0C, 0x00, 0x1A, 0x00,
+                        0x01, 0x00, 0x08, 0x00,
+                        0x03, 0x00, 0x00, 0x01,
+                        0x05, 0x01, 0x00, 0x00,
+                        0x00, 0x80, 0x01, 0x00,
+                        0x00, 0x10, 0x03, 0x10,
+                        0x3F, 0x00, 0x2A, 0x00};
+    int ret;
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    ret = decode_comp_data(&head, &data, mystr, sizeof(mystr));
+    if (ret == -1) {
+        printf("decode_comp_data error");
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
